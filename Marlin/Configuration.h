829bd3947bb5d429278e8b87156830a1244cca87
Milad Emadi <milademadi74@gmai.com>
2023-12-13 19:54:44 +0330

    Calibrate 1

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1226,8 +1226,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {155, 155, 5900, 200 }
-
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {160, 160, 6200, 200 }
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
  * Override with M203
